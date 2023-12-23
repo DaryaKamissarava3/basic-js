56c61280ddf82638d004d40b6b6a18d0f78b6597
Darya <dashakomiss37@gmail.com>
2023-12-23 11:18:37 +0300

    feat: add part of logic to some functions

--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,39 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined || !(date instanceof Date) || isNaN(date)) {
+    throw new Error('Unable to determine the time of year!');
+  }
+
+  const month = date.getMonth() + 1;
+
+  const seasonObj = {
+    1: 'winter',
+    2: 'winter',
+    3: 'spring',
+    4: 'spring',
+    5: 'spring',
+    6: 'summer',
+    7: 'summer',
+    8: 'summer',
+    9: 'autumn',
+    10: 'autumn',
+    11: 'autumn',
+    12: 'winter'
+  };
+
+  return seasonObj[month];
 }
 
 module.exports = {
